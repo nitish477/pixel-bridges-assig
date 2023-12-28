aed55ce8603ba6b601bbfddafcfaaab5d5cc028d
Nitish kumar <krnitish540@gmail.com>
2023-12-28 10:48:24 +0530

    Make Navbar

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import menu from './../../asset/menu.png'
+import connect from './../../asset/connect-icon.png'
+import { Link } from 'react-router-dom'
+import close from './../../asset/close.png'
+import "./Navbar.css"
+const ToggleSidebar = () => {
+    const [isOpen, setIsopen] = useState(false);
+
+    const ToggleSidebar = () => {
+        isOpen === true ? setIsopen(false) : setIsopen(true);
+    }
+    return (
+        <>
+            <div className="container-fluid mt-3">
+
+                <nav className="navbar navbar-expand-lg navbar-light ">
+                    <div className="container-fluid p-2">
+                        <div onClick={ToggleSidebar} >
+                            <img className="menu-btn" src={menu} />
+                        </div>
+                        <a className="navbar-brand text-light logo"><img src='https://app.lexer.markets/assets/logo_LEXER-80aa9bef.svg' className='logo-img' /></a>
+                        <div className="form-inline ml-auto">
+                            <button className='connect-btn'>
+                                <img src={connect} /> Connect</button>
+                        </div>
+                    </div>
+                </nav>
+                <div className={`sidebar ${isOpen == true ? 'active' : ''}`}>
+                    <div className="sd-header">
+                        <h4 className="mb-0"><img src='https://app.lexer.markets/assets/logo_LEXER-80aa9bef.svg' className='logo-img' /></h4>
+                        <div className="btn" onClick={ToggleSidebar}>
+                            <img className="close-btn" src={close} />
+                        </div>
+                    </div>
+                    <div className="sd-body">
+                        <ul>
+                            <li><Link to="/" className="mb-0 sd-link">Dashboard</Link></li>
+                            <li><Link to="#" className="sd-link">Migrate V1 Assets</Link></li>
+                            <li><Link to="/lexstack" className="sd-link">LexStake</Link></li>
+                            <li><Link to="#" className="sd-link">Liquidty</Link></li>
+                            <li><Link to="/Trade" className="sd-link">Trade</Link></li>
+                            <li><Link to="/leaderboard" className="sd-link">Leaderboard</Link></li>
+                            <li><Link to="#" className="sd-link">V2 Launch</Link></li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div className={`sidebar-overlay ${isOpen == true ? 'active' : ''}`} onClick={ToggleSidebar}></div>
+            </div>
+
+        </>
+    )
+}
+export default ToggleSidebar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LexStack from './views/LexStack/LexStack';
+import Dashboard from './views/Dashboard/Dashboard';
+import LeaderBoard from "./views/LeaderBoard/LeaderBoard"
+import Liquidity from './views/Liquidity/Liquidity';
+import Trade from './views/Trade/Trade';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path:"/lexstack",
+    element: <LexStack />
+  },
+  {
+    path: "/leaderboard",
+    element: <LeaderBoard />
+  },
+  {
+    path: "/trade",
+    element: <Trade />
+  },
+  {
+    path: "/liquidity",
+    element: <Liquidity />
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
