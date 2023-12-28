61f4a4240b35c314431a3f4526d76d347cbddba2
Nitish kumar <krnitish540@gmail.com>
2023-12-28 10:54:45 +0530

    Make a Dash boardCard

---
+++ src/component/DashboardCard/DashboardCard.js
@@ -0,0 +1,15 @@
+import "./DashboardCard.css"
+
+export default function DashboardCard({ title, number }) {
+    return (
+        <>
+
+            <div className="card-1">
+                <h5 className="title">{title}</h5>
+                <p className="number">{number}</p>
+            </div>
+
+        </>
+
+    );
+}
